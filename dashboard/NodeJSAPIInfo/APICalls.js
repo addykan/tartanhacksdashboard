543ff018fb7eeef64c8d7603883f613c302abe05
Adhvik Kanagala <52569612+addykan@users.noreply.github.com>
2020-10-17 11:24:25 -0400

    Learning Node stuff

---
+++ dashboard/NodeJSAPIInfo/APICalls.js
@@ -0,0 +1,7 @@
+const request = require('E:\General\NodeJS\node_modules\request\request.js');
+
+request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body.url);
+  console.log(body.explanation);
+});
